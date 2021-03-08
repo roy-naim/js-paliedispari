5baed58a944eaf8d50f5c1a2df77031b149e2817
Roy <b.roynaim@gmail.com>
2021-03-08 19:17:46 +0100

    Palindroma esercizio completato

--- main.js
+++ main.js
@@ -1,28 +1,26 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+var word = prompt('Inserisci una parola:');
+
+var newWord = verify(word);
 
 
-
-
-
-function verify(){
-  var word = prompt('Inserisci una parola:');
-  // The split() method is used to split a string into an array of substrings,
- // and returns the new array.
-  var singleWord = word.split('');
-
-  //reverse that word u put
-  var reverseWord = word.reverse();
-
-  // join for creating new word
-  var joinWord = word.join('');
-
-  if (word === joinWord) {
-    alert("La parola è palindroma");
-  } else {
-    alert("Non e' palindroma");
-    console.log(word);
-  }
+if (word == newWord) {
+  alert("La parola è palindroma");
+} else {
+  alert("Non e' palindroma");
 }
 
-verify();
+function verify(word) {
+  // The split() method is used to split a string into an array of substrings,
+ // and returns the new array.
+  var newWord = word.split('').reverse().join('');
+  // var singleWord = word.split('');
+  //
+  // //reverse that word u put
+  // var reverseWord = word.reverse();
+  //
+  // // join for creating new word
+  // var joinWord = word.join('');
+  return newWord;
+}
